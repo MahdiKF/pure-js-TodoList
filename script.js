b3c32b3c0144786ebceaf1241c9e0bb746d7f475
MahdiKF <kashfimahdi90@gmail.com>
2026-08-28 21:35:31 +0330

    fix remove item bugs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const inputInvalid = document.getElementById("input-invalid");
-const itemList = document.getElementById("item-list")
-const clearBtn = document.getElementById("items-clear")
- 
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("items-clear");
+const itemFilter = document.getElementById("filter");
+
 function addItem(e) {
   e.preventDefault();
   const newItem = itemInput.value;
@@ -12,37 +13,51 @@ function addItem(e) {
     return;
   } else {
     inputInvalid.innerText = "";
+    checkUI();
   }
 
   const li = document.createElement("li");
   li.className = "list-item";
   li.textContent = newItem;
-  
-  const icon = document.createElement('i');
-  icon.className = 'bi bi-x fs-5 text-danger'
 
-  li.appendChild(icon)
-  itemList.appendChild(li)
+  const icon = document.createElement("i");
+  icon.className = "bi bi-x fs-5 text-danger";
 
-  itemInput.value= ""
+  li.appendChild(icon);
+  itemList.appendChild(li);
+
+  itemInput.value = "";
 }
 
-
-function onClickItem(e){
-    
-    if(e.target.classList.contains('bi-x')){
-        e.target.parentElement.remove();
-    }
+function onClickItem(e) {
+  if (e.target.classList.contains("bi-x")) {
+    e.target.parentElement.remove();
+    checkUI();
+  }
 }
 
-function onClickClear () {
-
-    itemList.innerHTML = ''
-    
-
+function onClickClear() {
+  itemList.innerHTML = "";
+  checkUI();
 }
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  }else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
 // Event Listener
 
 itemForm.addEventListener("submit", addItem);
-itemList.addEventListener('click' , onClickItem)
-clearBtn.addEventListener('click', onClickClear )
+itemList.addEventListener("click", onClickItem);
+clearBtn.addEventListener("click", onClickClear);
+
+
+checkUI();
